aca772352345b6f85ca41aaafcb25c96c966038b
danddr <dare.daniele@gmail.com>
2023-01-27 10:11:06 +0100

    add parallelized version of optimRes

--- R/optimRes.R
+++ R/optimRes.R
@@ -12,37 +12,43 @@
 #' @param sdf an sf object having point geometry given by the PC-scores values
 #' @param grid.res (integer) a vector of resolutions to be tested, i.e seq(1,100, by=1)
 #' @param perc.thr rate of change (expressed in percentage) of the function to be minimized for selecting the optimal resolution.  
+#' @param cr (integer) number of cores for parallel computing.
 #' @param showOpt (logical) plot the result. 
 #' @return It returns a list with: i) a matrix reporting the values of the function to be minimized, along with the corresponding resolution; ii) the optimal resolution.
 #' @export
-optimRes <- function(sdf, grid.res, perc.thr = 10, showOpt = TRUE) {
-   stopifnot(exprs = {
+optimRes <- function(sdf, grid.res, perc.thr = 10, cr = 1, showOpt = TRUE) {
+  stopifnot(exprs = {
     is.numeric(perc.thr)
     is.numeric(grid.res)
     is.logical(showOpt)
     inherits(sdf, "sf")
   })
+  if(is.null(cr)) stop("Please, provide a cluster")
   grid.res <- sort(grid.res, decreasing = FALSE)
-  SS_vec <- sapply(grid.res, function(res) { 
+  SS_vec <- parSapply(cr, grid.res, function(res) {
     Grd <- st_make_grid(sdf, n = res)
     SS_mean <- mean(sapply(Grd, function(i) {
       X <- sdf[i, ]
-      if(nrow(X) >= 2) {
+      if (nrow(X) >= 2) {
         X_c <- st_centroid(st_convex_hull(st_union(X)))
-        D <- sum(as.numeric(st_distance(X, X_c))^2)/nrow(X) 
+        D <- sum(as.numeric(st_distance(X, X_c))^2)/nrow(X)
         return(D)
-      } else {
+      }
+      else {
         return(NA_real_)
       }
     }), na.rm = TRUE)
     return(SS_mean)
   })
+  stopCluster(cr) #stop clusters used in parSapply
   SS_vec <- sqrt(SS_vec)
-  Rat_chg <- SS_vec[1:(length(SS_vec)-1)]/SS_vec[2:length(SS_vec)]
-  Res_opt <- grid.res[(which(Rat_chg <= ((perc.thr/100)+1))[1])] 
-  if(showOpt) {
-    plot(grid.res, SS_vec, type = "b", xlab = "Grid resolution", ylab = "Function", main = "Optimal grid resolution")
+  Rat_chg <- SS_vec[1:(length(SS_vec) - 1)]/SS_vec[2:length(SS_vec)]
+  Res_opt <- grid.res[(which(Rat_chg <= ((perc.thr/100) + 1))[1])]
+  if (showOpt) {
+    plot(grid.res, SS_vec, type = "b", xlab = "Grid resolution", 
+         ylab = "Function", main = "Optimal grid resolution")
     abline(v = Res_opt, col = "red")
   }
-  return(list(F_val = cbind(Fval = SS_vec, Res = grid.res), Opt_res = Res_opt))
+  return(list(F_val = cbind(Fval = SS_vec, Res = grid.res), 
+              Opt_res = Res_opt))
 }
