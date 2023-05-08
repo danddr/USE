2bf05ce9e92ee88f31813c40d64a90b148080326
danddr <dare.daniele@gmail.com>
2023-05-08 10:50:11 +0200

    fix error in the template rasters

--- R/Worldclim_tmp.R
+++ R/Worldclim_tmp.R
@@ -6,6 +6,6 @@
 #' @keywords datasets
 #' @name Worldclim_tmp
 #' @usage data(Worldclim_tmp)
-#' @format A RasterLayer with 1080 rows, 2160 columns, and 5 layers, namely: wc2.1_10m_bio_4, wc2.1_10m_bio_3, wc2.1_10m_bio_14, wc2.1_10m_bio_9, wc2.1_10m_bio_15.
+#' @format A data frame obtained from a SpatRaster with 1080 rows, 2160 columns, and 5 layers, namely: wc2.1_10m_bio_4, wc2.1_10m_bio_3, wc2.1_10m_bio_14, wc2.1_10m_bio_9, wc2.1_10m_bio_15.
 #' @source \code{geodata::worldclim_global(var='bio', res=10, path=getwd())[[c(4,  3, 14,  9, 15)]]} 
 'Worldclim_tmp'
